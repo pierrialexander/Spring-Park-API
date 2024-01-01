f617646122d6a75856fe5e865cc3699c4523f988
Pierri Alexander Vidmar <pierrialexandervidmar@gmail.com>
2023-12-31 23:46:03 -0300

    Implementado as rotas e métodos para criar usuário

--- src/main/java/com/sistema/parkapi/service/UsuarioService.java
+++ src/main/java/com/sistema/parkapi/service/UsuarioService.java
@@ -1,9 +1,11 @@
 package com.sistema.parkapi.service;
 
+import com.sistema.parkapi.entity.Usuario;
 import com.sistema.parkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +15,8 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
 
 
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- src/main/java/com/sistema/parkapi/web/controller/UsuarioController.java
+++ src/main/java/com/sistema/parkapi/web/controller/UsuarioController.java
@@ -1,9 +1,16 @@
 package com.sistema.parkapi.web.controller;
 
+import com.sistema.parkapi.entity.Usuario;
 import com.sistema.parkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,5 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        Usuario user = usuarioService.salvar(usuario);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
 
 }
